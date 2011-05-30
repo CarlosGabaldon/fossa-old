fa82e27479560e44612afdbe518d11e31071c926
Carlos Gabaldon <cgabaldon@gmail.com>
2011-05-29 21:24:16 -0700

    project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fossa::Application.config.secret_token = '7b060c89370a1bba891b08cf56aae26adc3eb6b45fd8e891d36ccbeafd0ae2a26076d4b53003566f8934a8d821df6288ff129c916b0a5c8f6511687b427be6bc'
